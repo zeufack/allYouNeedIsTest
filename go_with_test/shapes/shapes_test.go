a515996247c20e44f06d3091e9bcf1ab46f5dca0
zeufack <zeufackp@gmail.com>
2024-03-26 21:45:54 +0100

    add table driver test

--- go_with_test/shapes/shapes_test.go
+++ go_with_test/shapes/shapes_test.go
@@ -14,33 +14,48 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
 		}
 	}
 
-	t.Run("Rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-		// got := rectangle.Area()
-		// want := 72.0
+	// checkArea := func(t testing.TB, shape Shape, want float64) {
+	// 	t.Helper()
+	// 	got := shape.Area()
+	// 	if got != want {
+	// 		t.Errorf("got %g want %g", got, want)
+	// 	}
+	// }
 
-		// if got != want {
-		// 	t.Errorf("got %.2f want %2.f", got, want)
-		// }
-	})
+	// t.Run("Rectangles", func(t *testing.T) {
+	// 	rectangle := Rectangle{12.0, 6.0}
+	// 	checkArea(t, rectangle, 72.0)
+	// 	// got := rectangle.Area()
+	// 	// want := 72.0
 
-	t.Run("Circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-		// got := circle.Area()
-		// want := 314.1592653589793
+	// 	// if got != want {
+	// 	// 	t.Errorf("got %.2f want %2.f", got, want)
+	// 	// }
+	// })
 
-		// if got != want {
-		// 	t.Errorf("got %g want %g", got, want)
-		// }
-	})
+	// t.Run("Circles", func(t *testing.T) {
+	// 	circle := Circle{10}
+	// 	checkArea(t, circle, 314.1592653589793)
+	// 	// got := circle.Area()
+	// 	// want := 314.1592653589793
+
+	// 	// if got != want {
+	// 	// 	t.Errorf("got %g want %g", got, want)
+	// 	// }
+	// })
 }
