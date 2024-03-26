266534b6a2554e32869101f2bf018820d6a9352b
zeufack <zeufackp@gmail.com>
2024-03-26 20:40:36 +0100

    using struct and method

---
+++ go_with_test/shapes/shapes.go
@@ -0,0 +1,28 @@
+package shapes
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Height + rectangle.Width)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
---
+++ go_with_test/shapes/shapes_test.go
@@ -0,0 +1,36 @@
+package shapes
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %2.f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+	t.Run("Rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := rectangle.Area()
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got %.2f want %2.f", got, want)
+		}
+	})
+
+	t.Run("Circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+}
