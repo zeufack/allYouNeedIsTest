16b5d53add92e2826d7d8652d2bc1782bab3de3b
zeufack <zeufackp@gmail.com>
2024-03-28 19:09:01 +0100

    add helper function

--- go_with_test/pointers/wallet.go
+++ go_with_test/pointers/wallet.go
@@ -3,14 +3,18 @@ package pointers
 import "fmt"
 
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(balance int) {
+func (w *Wallet) Deposit(balance Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
 	w.balance += balance
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
--- go_with_test/pointers/wallet_test.go
+++ go_with_test/pointers/wallet_test.go
@@ -1,19 +1,46 @@
 package pointers
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
 
-	got := wallet.Balance()
-	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
-	want := 10
-
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.balance
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		// got := wallet.Balance()
+		// fmt.Printf("address of balance in test is %p \n", &wallet.balance)
+		// want := Bitcoin(10)
+
+		// if got != want {
+		// 	t.Errorf("got %s want %s", got, want)
+		// }
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		// got := wallet.Balance()
+
+		// want := Bitcoin(10)
+
+		// if got != want {
+		// 	t.Errorf("got %s want %s", got, want)
+		// }
+		assertBalance(t, wallet, Bitcoin(10))
+
+	})
+
 }
