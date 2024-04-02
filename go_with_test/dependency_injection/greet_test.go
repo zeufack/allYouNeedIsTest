4d6fe7b77963e3c83eaf67feaeb3f3edf3ff25f5
zeufack <zeufackp@gmail.com>
2024-04-02 23:50:27 +0200

    take a look of dependencies injection

---
+++ go_with_test/dependency_injection/greet_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreed(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Chris")
+
+	got := buffer.String()
+	want := "Hello, Chris"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
