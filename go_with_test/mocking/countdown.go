fa33a357b18298d091408f5495295a3f8f049251
zeufack <zeufackp@gmail.com>
2024-04-04 20:38:45 +0200

    complete countdown prog

--- go_with_test/mocking/countdown.go
+++ go_with_test/mocking/countdown.go
@@ -19,11 +19,45 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+const write = "write"
+const sleep = "sleep"
+
 var finalWord = "Go!"
 
 const countdownStart = 3
 
 func Countdown(out io.Writer, slepper Sleeper) {
+
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 		slepper.Sleep()
