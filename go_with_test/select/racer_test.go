ec9e26d74d17ebd45372bbd16319c5de7f4886d5
zeufack <zeufackp@gmail.com>
2024-04-06 23:08:17 +0200

    use defer to  set intruction execution order to befor the end of the function

---
+++ go_with_test/select/racer_test.go
@@ -0,0 +1,43 @@
+package selects
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	//  httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 	time.Sleep(20 * time.Millisecond)
+	// 	w.WriteHeader(http.StatusOK)
+	// }))
+
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+	// httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 	w.WriteHeader(http.StatusOK)
+	// }))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
