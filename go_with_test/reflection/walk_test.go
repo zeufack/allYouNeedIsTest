dc20329969aeaabe0a2f25d9a15a09756a933337
zeufack <zeufackp@gmail.com>
2024-04-08 21:51:26 +0200

    refactor code to make it work with array of many string

--- go_with_test/reflection/walk_test.go
+++ go_with_test/reflection/walk_test.go
@@ -1,24 +1,61 @@
 package reflection
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalk(t *testing.T) {
-	expected := "Chris"
-	var got []string
 
-	x := struct {
-		Name string
-	}{expected}
-
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		expectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
 	}
 
-	if got[0] != expected {
-		t.Errorf("got %q, want %q", got[0], expected)
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+			if !reflect.DeepEqual(got, test.expectedCalls) {
+				t.Errorf("got %v, want %v", got, test.expectedCalls)
+			}
+		})
 	}
+	// expected := "Chris"
+	// var got []string
+
+	// x := struct {
+	// 	Name string
+	// }{expected}
+
+	// walk(x, func(input string) {
+	// 	got = append(got, input)
+	// })
+
+	// if len(got) != 1 {
+	// 	t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	// }
+
+	// if got[0] != expected {
+	// 	t.Errorf("got %q, want %q", got[0], expected)
+	// }
 }
