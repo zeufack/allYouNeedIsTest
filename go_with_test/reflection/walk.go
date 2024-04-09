5388d2f6977da9bd01308c17b87f2de90c5443bc
zeufack <zeufackp@gmail.com>
2024-04-09 21:53:10 +0200

    make function work for slice

--- go_with_test/reflection/walk.go
+++ go_with_test/reflection/walk.go
@@ -5,19 +5,42 @@ import "reflect"
 func walk(x interface{}, fun func(input string)) {
 	val := getValue(x)
 
-	if val.Kind() == reflect.Pointer {
-		val = val.Elem()
-	}
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
+	numberOfValue := 0
+	var getField func(int) reflect.Value
 
-		switch field.Kind() {
-		case reflect.String:
-			fun(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fun)
-		}
+	switch val.Kind() {
+	case reflect.Struct:
+		numberOfValue = val.NumField()
+		getField = val.Field
+	case reflect.Slice:
+		numberOfValue = val.Len()
+		getField = val.Index
+	case reflect.String:
+		fun(val.String())
 	}
+
+	for i := 0; i < numberOfValue; i++ {
+		walk(getField(i).Interface(), fun)
+	}
+	// if val.Kind() == reflect.Slice {
+	// 	for i := 0; i < val.Len(); i++ {
+	// 		walk(val.Index(i).Interface(), fun)
+	// 	}
+	// 	return
+	// }
+	// if val.Kind() == reflect.Pointer {
+	// 	val = val.Elem()
+	// // }
+	// for i := 0; i < val.NumField(); i++ {
+	// 	field := val.Field(i)
+
+	// 	switch field.Kind() {
+	// 	case reflect.String:
+	// 		fun(field.String())
+	// 	case reflect.Struct:
+	// 		walk(field.Interface(), fun)
+	// 	}
+	// }
 
 }
 
