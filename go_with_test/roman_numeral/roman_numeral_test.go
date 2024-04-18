10cebca9aa5d6b64e65eb4ce79ef75b64cb999b7
zeufack <zeufackp@gmail.com>
2024-04-18 17:51:37 +0200

    update roman numeral for base cases

--- go_with_test/roman_numeral/roman_numeral_test.go
+++ go_with_test/roman_numeral/roman_numeral_test.go
@@ -11,6 +11,15 @@ func TestRomanNumerals(t *testing.T) {
 		{"1 gets converted to I", 1, "I"},
 		{"2 gets converted to II", 2, "II"},
 		{"3 gets converted to III", 3, "III"},
+		{"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
+		{"6 gets converted to VI", 6, "VI"},
+		{"9 gets converted to IX", 9, "IX"},
+		{"10 gets converted to X", 10, "X"},
+		{"14 gets converted to XIV", 14, "XIV"},
+		{"18 gets converted to XVIII", 18, "XVIII"},
+		{"20 gets converted to XX", 20, "XX"},
+		{"39 gets converted to XXXIX", 39, "XXXIX"},
 	}
 
 	for _, test := range cases {
